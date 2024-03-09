8afd5c84688179a3d3e316b9898a9f4fb367ee72
andreytkach85 <andreytkach85@gmail.com>
2024-03-09 17:32:50 +0100

    756fsdfds

---
+++ CityInfo/CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,82 @@
+﻿using CityInfo.API;
+using CityInfo.API.Models;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public List<CityDto> Cities { get; set; }
+
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description= "The one with that big park.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Central Park",
+                            Description = "The most visited urban park in the United States."
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "Empire State Building",
+                            Description = "A 102-story skyscrapper located in Midtown Manhattan."
+                        }
+                    }
+                },
+                new CityDto
+                {
+                    Id = 2,
+                    Name = "Antwerp",
+                    Description= "The one with the cathedral that was never really finished.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 3,
+                            Name = "Name3",
+                            Description = "Description3"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 4,
+                            Name = "Name4",
+                            Description = "Description4"
+                        }
+                    }
+                },
+                new CityDto
+                {
+                    Id = 3,
+                    Name = "Paris",
+                    Description= "The one with that big tower.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 5,
+                            Name = "Name5",
+                            Description = "Description5"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 5,
+                            Name = "Name5",
+                            Description = "Description5"
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
